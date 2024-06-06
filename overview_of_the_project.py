835c53da27a041189259e30541408d44c2e4106f
Wojciech Bednarz <wojciech.bednarz@nokia.com>
2024-06-06 15:59:21 +0200

    First commit

---
+++ overview_of_the_project.py
@@ -0,0 +1,7 @@
+# REST API
+# Create stores, each with a name and list of stocked items.
+# Retrieve a list of all stores and their items.
+# Given its name, retrieve an individual store and all its items.
+# Given a store name, retrieve only a list of item within it.
+
+#https://rest-apis-flask.teclado.com/docs/first_rest_api/project_overview/
